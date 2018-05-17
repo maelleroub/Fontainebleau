cc2f98afdf2d1b69b35ffbb589f9466b65650e98
maelle.roubeau <maelle.roubeau@epita.fr>
2018-05-17 15:14:34 +0200

    Vector: Now holds *void

--- vector.c
+++ vector.c
@@ -3,74 +3,83 @@
 struct vector* vector_create(size_t c)
 {
   struct vector *v = malloc(sizeof(struct vector));
-  v->data = malloc(c * sizeof(int));
+  v->data = malloc(c * sizeof(void*));
   v->capacity = c;
   v->size = 0;
   return v;
 }
 
-void vector_insert(struct vector *v, int a)
+int vector_is_empty(struct vector *v)
 {
-  if(v->size >= v->capacity)
-    return;
-  size_t i = 0;
-  while(i < v->size && a > v->data[i])
-    i++;
-  if(i < v->capacity)
-  {
-    for(size_t j = v->size; j > i; j--)
-      v->data[j] = v->data[j - 1];
-    v->data[i] = a;
-    v->size++;
-  }
+  return !v->size;
 }
 
-void vector_delete(struct vector *v, int a)
+void vector_insert_at(struct vector *v, size_t a, void *x)
 {
-  if(!v->size)
+  if(v->size >= v->capacity || a > v->size)
     return;
-  size_t i = 0;
-  while(i < v->size && a > v->data[i])
-    i++;
-  if(i < v->size && v->data[i] == a)
-  {
-    for(size_t j = i; j < v->size - 1; j++)
-      v->data[j] = v->data[j + 1];
-    v->size--;
-  }
+  for(size_t j = v->size; j > a; j--)
+    v->data[j] = v->data[j - 1];
+  v->data[a] = x;
+  v->size++;
+}
+
+void vector_delete_at(struct vector *v, size_t a)
+{
+  if(a >= v->size)
+    return;
+  for(size_t j = a; j < v->size - 1; j++)
+    v->data[j] = v->data[j + 1];
+  v->size--;
 }
 
 void vector_print_int(struct vector *v)
 {
+  if(vector_is_empty(v))
+    return;
   for(size_t i = 0; i < v->size - 1; i++)
-  {
-    printf("%d -> ", v->data[i]);
-  }
-  printf("%d\n", v->data[v->size - 1]);
+    printf("%d -> ", *(int*)v->data[i]);
+  printf("%d\n", *(int*)v->data[v->size - 1]);
 }
 
 int main()
 {
   struct vector *v = vector_create(5);
-  vector_insert(v, 3);
+  int* a = malloc(sizeof(int));
+  *a = 3;
+  int* b = malloc(sizeof(int));
+  *b = 1;
+  int* c = malloc(sizeof(int));
+  *c = 4;
+  int* d = malloc(sizeof(int));
+  *d = 2;
+  int* e = malloc(sizeof(int));
+  *e = 7;
+  vector_insert_at(v, 0, a);
   vector_print_int(v);
-  vector_insert(v, 1);
+  vector_insert_at(v, 1, b);
   vector_print_int(v);
-  vector_insert(v, 4);
+  vector_insert_at(v, 0, c);
   vector_print_int(v);
-  vector_insert(v, 2);
+  vector_insert_at(v, 0, d);
   vector_print_int(v);
-  vector_insert(v, 7);
+  vector_insert_at(v, 2, e);
   vector_print_int(v);
-  vector_delete(v, 4);
+  vector_delete_at(v, 4);
   vector_print_int(v);
-  vector_delete(v, 42);
+  vector_delete_at(v, 0);
   vector_print_int(v);
-  vector_insert(v, 0);
+  vector_delete_at(v, 0);
   vector_print_int(v);
-  vector_delete(v, 0);
+  vector_delete_at(v, 0);
   vector_print_int(v);
-  vector_insert(v, 3);
+  vector_delete_at(v, 0);
+  vector_print_int(v);
+  vector_delete_at(v, 0);
+  vector_print_int(v);
+  vector_delete_at(v, 0);
+  vector_print_int(v);
+  vector_delete_at(v, 0);
   vector_print_int(v);
   return 1;
 }
--- vector.h
+++ vector.h
@@ -6,11 +6,19 @@
 
 struct vector
 {
-  int *data;
+  void **data;
   size_t capacity;
   size_t size;
 };
 
 struct vector* vector_create(size_t c);
+
+int vector_is_empty(struct vector *v);
+
+void vector_insert_at(struct vector *v, size_t a, void *x);
+
+void vector_delete_at(struct vector *v, size_t a);
+
+void vector_print_int(struct vector *v);
 
 #endif
